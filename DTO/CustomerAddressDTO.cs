e67a56ecd017c724b5f49807857354a9a2ff1f71
Khin Su Mon <sumonkk@gmail.com>
2024-11-21 10:57:28 +0630

    Add files via upload

---
+++ DTO/CustomerAddressDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomersMasterDetail.DTO
+{
+    public class CustomerAddressDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
---
+++ DTO/SoftwareExperienceDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace CustomersMasterDetail.DTO
+{
+    public class SoftwareExperienceDTO
+    {
+        public int Id { get; set; }
+        public int ApplicantId { get; set; }
+        public int SoftwareId { get; set; }
+        public int Rating { get; set; }
+        public string Notes { get; set; }
+        public bool IsHidden { get; set; } = false;
+    }
+}
